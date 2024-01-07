c5b33b342689d68e79172209624d7dff1194d080
Nate Sales <nate@natesales.net>
2024-01-06 23:22:36 -0500

    feat: add turbo control

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+
+from turbo import TCP015Controller
+
+turbo = TCP015Controller("/dev/ttyUSB0")
+# turbo.off()
+
+print(f"Connected to turbo pump at {turbo.ser}, firmware version {turbo.firmware_version()}")
+
+if not turbo.is_running():
+    print("Starting pump station in")
+    for i in range(3, 0, -1):
+        print(i)
+        time.sleep(1)
+    turbo.on()
+
+while True:
+    print(f"RPM: {turbo.rpm()}, current draw: {turbo.current_draw()}")
+    time.sleep(1)
