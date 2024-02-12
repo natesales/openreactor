a1e59e7ba88a85d0011754dbaccd76e19eb76bca
Nate Sales <nate@natesales.net>
2024-02-12 18:59:33 -0500

    refactor: cleanup

--- cmd/mfc/main.go
+++ cmd/mfc/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"net/http"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -15,15 +14,6 @@ var (
 	verbose       = flag.Bool("v", false, "Enable verbose logging")
 	trace         = flag.Bool("trace", false, "Enable trace logging")
 )
-
-func exec(f func() error) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(); err != nil {
-			w.Write([]byte("Error: " + err.Error()))
-		}
-		w.Write([]byte("ok"))
-	}
-}
 
 func main() {
 	flag.Parse()
--- cmd/turbo/main.go
+++ cmd/turbo/main.go
@@ -8,6 +8,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/natesales/openreactor/db"
+	"github.com/natesales/openreactor/util"
 )
 
 var (
@@ -17,15 +18,6 @@ var (
 	verbose        = flag.Bool("v", false, "Enable verbose logging")
 	trace          = flag.Bool("trace", false, "Enable trace logging")
 )
-
-func exec(f func() error) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(); err != nil {
-			w.Write([]byte("Error: " + err.Error()))
-		}
-		w.Write([]byte("ok"))
-	}
-}
 
 func main() {
 	flag.Parse()
@@ -51,8 +43,8 @@ func main() {
 	}
 	log.Infof("Turbo pump %s", fw)
 
-	http.HandleFunc("/turbo/on", exec(t.On))
-	http.HandleFunc("/turbo/off", exec(t.Off))
+	http.HandleFunc("/turbo/on", util.HandleExec(t.On))
+	http.HandleFunc("/turbo/off", util.HandleExec(t.Off))
 
 	log.Infof("Starting API on %s", *apiListen)
 	go http.ListenAndServe(*apiListen, nil)
