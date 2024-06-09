6d19d07df0d2e261f96085012972e56da770d496
Nate Sales <nate@natesales.net>
2024-06-09 19:54:30 -0400

    feat: add neutron counter

---
+++ cmd/counter/counter.ino
@@ -0,0 +1,21 @@
+#define INTERRUPT_PIN 15
+
+uint32_t counts = 0;
+uint16_t period = 1000;
+
+void setup() {
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), count, FALLING);
+  Serial.begin(115200);
+}
+
+void count() {
+  counts++;
+}
+
+void loop() {
+  delay(period);
+  detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));  
+  Serial.println(counts);
+  counts = 0;
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), count, FALLING);
+}
