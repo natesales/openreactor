003e9985583cd1525d877b53e8c9c86131ad89ef
Nate Sales <nate@natesales.net>
2024-06-11 16:38:48 -0400

    refactor(hv): convert to service

--- cmd/hv/main.go
+++ cmd/hv/main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"net/http"
 	"strconv"
-	"time"
 
+	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/natesales/openreactor/pkg/alert"
 	"github.com/natesales/openreactor/pkg/db"
-	"github.com/natesales/openreactor/pkg/serial"
-)
-
-var (
-	serialPort   = flag.String("s", "/serial", "Serial port")
-	apiListen    = flag.String("l", ":80", "API listen address")
-	pushInterval = flag.Duration("i", 1*time.Second, "Metrics push interval")
-	verbose      = flag.Bool("v", false, "Enable verbose logging")
-	trace        = flag.Bool("trace", false, "Enable trace logging")
+	"github.com/natesales/openreactor/pkg/service"
 )
 
 func encode(msg string) []byte {
@@ -27,33 +17,12 @@ func encode(msg string) []byte {
 }
 
 func main() {
-	flag.Parse()
-	if *verbose {
-		log.SetLevel(log.DebugLevel)
-		log.Debug("Debug logging enabled")
-	}
-	if *trace {
-		log.SetLevel(log.TraceLevel)
-		log.Trace("Trace logging enabled")
-	}
+	svc := service.New(115200)
 
-	c := serial.New(*serialPort, 115200)
-	// t := TCP015{serial.New(*serialPort, 115200)}
-	log.Infof("Connecting to HV controller on %s", c.Port)
-	if err := c.Connect(); err != nil {
-		log.Fatal(err)
-	}
-
-	// ver, err := m.Version()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Infof("MFC version %s", ver)
-
-	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
-		v, err := strconv.ParseFloat(r.URL.Query().Get("v"), 64)
+	svc.App.Get("/set", func(ctx *fiber.Ctx) error {
+		v, err := strconv.ParseFloat(ctx.Query("v"), 64)
 		if err != nil {
-			w.Write([]byte(fmt.Sprintf("error parsing slpm URL param: %v", err)))
+			return ctx.SendString(fmt.Sprintf("error parsing slpm URL param: %v", err))
 		}
 		if v == 0 {
 			alert.Alert("Disabling HV supply")
@@ -61,38 +30,33 @@ func main() {
 			alert.Alert(fmt.Sprintf("Setting voltage to %.4f", v))
 		}
 		log.Infof("Setting voltage to %f", v)
-		resp, err := c.Send(encode(fmt.Sprintf("s%d", int(v*1000))))
+		resp, err := svc.SerialPort.Send(encode(fmt.Sprintf("s%d", int(v*1000))))
 		if err != nil {
-			w.Write([]byte(fmt.Sprintf("error setting voltage: %v", err)))
-			return
+			return ctx.SendString(fmt.Sprintf("error setting voltage: %v", err))
 		}
 		if err := db.Write("hv_setpoint", nil, map[string]any{"v": v}); err != nil {
 			log.Warn(err)
 		}
-		w.Write([]byte(resp))
+		return ctx.SendString(resp)
 	})
 
-	log.Infof("Starting API on %s", *apiListen)
-	go http.ListenAndServe(*apiListen, nil) // TODO: Error logging
-
-	log.Infof("Starting metrics reporter every %s", *pushInterval)
-	ticker := time.NewTicker(*pushInterval)
-	for ; true; <-ticker.C {
-		v, err := c.Send(encode("r"))
+	svc.SetPoller(func() error {
+		v, err := svc.SerialPort.Send(encode("r"))
 		if err != nil {
-			log.Warnf("getting voltage: %v", err)
-			continue
+			return fmt.Errorf("getting voltage: %v", err)
 		}
 		voltage, err := strconv.ParseFloat(v, 64)
 		if err != nil {
-			log.Warnf("parsing voltage %s: %v", v, err)
-			continue
+			return fmt.Errorf("parsing voltage %s: %v", v, err)
 		}
 		voltage *= 1000
 		log.Debugf("Voltage: %f", voltage)
 		if err := db.Write("hv_voltage", nil, map[string]any{"v": voltage}); err != nil {
-			log.Warn(err)
-			continue
+			return err
 		}
-	}
+
+		return nil
+	})
+
+	svc.Start()
 }
