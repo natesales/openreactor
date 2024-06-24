61bc8116b9ec568a0f85d6ad4bfa98d5fdf12dd8
Nate Sales <nate@natesales.net>
2024-06-24 01:47:47 -0400

    refactor(ui): use color variables for Dot component

--- ui/src/lib/ConnectionChip.svelte
+++ ui/src/lib/ConnectionChip.svelte
@@ -1,35 +1,17 @@
 <script>
+    import Dot from "./Dot.svelte";
+
     export let connected = false;
 </script>
 
 <main>
-    {#if connected}
-        <span class="dot connected"></span>
-        <span>Connected</span>
-    {:else}
-        <span class="dot disconnected"></span>
-        <span>Disconnected</span>
-    {/if}
+    <Dot --color="var(--{connected ? 'green' : 'red'})" />
+    <span>{connected ? "Connected" : "Disconnected"}</span>
 </main>
 
 <style>
     main {
         display: flex;
         align-items: center;
-    }
-
-    .dot {
-        height: 10px;
-        width: 10px;
-        border-radius: 50%;
-        margin-right: 5px;
-    }
-
-    .connected {
-        background-color: #0dbd0d;
-    }
-
-    .disconnected {
-        background-color: red;
     }
 </style>
---
+++ ui/src/lib/Dot.svelte
@@ -0,0 +1,11 @@
+<span></span>
+
+<style>
+    span {
+        height: 10px;
+        width: 10px;
+        border-radius: 50%;
+        margin-right: 5px;
+        background-color: var(--color, #565656);
+    }
+</style>
